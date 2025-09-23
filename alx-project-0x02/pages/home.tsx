024e2c7aebab11d895fb1d43f3724eb8d1202d23
olivier01A <oliviermasengesho590@gmail.com>
2025-09-23 15:43:07 +0200

    Create home.tsx

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      <p className="mt-2 text-gray-600">This is the home page of the site.</p>
+    </div>
+  );
+};
+
+export default HomePage;
