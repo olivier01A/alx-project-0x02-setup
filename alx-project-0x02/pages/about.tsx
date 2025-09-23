43d6aa3259cf7e0b34d00e08657b6af85b7a8d08
olivier01A <oliviermasengesho590@gmail.com>
2025-09-23 15:44:50 +0200

    Create about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className="mt-2 text-gray-600">This is the about page of the site.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
