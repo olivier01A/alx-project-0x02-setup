eb129de24156acf044d7bce454cc79a350ab8083
olivier01A <oliviermasengesho590@gmail.com>
2025-09-23 15:24:17 +0200

    Create index.tsx

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,11 @@
+// pages/index.tsx
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Welcome to our website!</h1>
+    </div>
+  );
+};
+
+export default Home;
