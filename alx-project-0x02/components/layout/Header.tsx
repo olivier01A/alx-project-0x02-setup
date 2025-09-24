6211f5b812198149f77459d1955795482787a325
olivier01A <oliviermasengesho590@gmail.com>
2025-09-24 19:27:45 +0200

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -18,3 +18,27 @@ const Header = () => {
 
 export default Header;
 
+
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-900 text-white px-6 py-4 shadow-md">
+      <nav className="flex space-x-6">
+        <Link href="/home" className="hover:text-gray-300">
+          Home
+        </Link>
+        <Link href="/about" className="hover:text-gray-300">
+          About
+        </Link>
+        <Link href="/posts" className="hover:text-gray-300">
+          Posts
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
+
+
