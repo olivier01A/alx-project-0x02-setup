d2527d79e5e9027296b2a24402005acdb872968c
olivier01A <oliviermasengesho590@gmail.com>
2025-09-24 19:33:10 +0200

    Create posts.tsx

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage() {
+  return (
+    <>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-2xl font-bold">Posts</h1>
+      </main>
+    </>
+  );
+}
