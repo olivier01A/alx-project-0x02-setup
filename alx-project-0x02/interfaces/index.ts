45f97d0f6f38f4482e9dbfcb75cc5200de093600
olivier01A <oliviermasengesho590@gmail.com>
2025-09-24 19:16:06 +0200

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -5,3 +5,11 @@ export interface CardProps {
   content: string;
 }
 
+export interface ButtonProps {
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+  children: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  className?: string;
+}
